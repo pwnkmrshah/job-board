1c50322900a18e259197ff18fbed23de8a8bd7b8
pawanshah <pwnkmrshah@gmail.com>
2021-11-03 15:22:51 +0530

    fix for  css issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import $ from 'jquery'
-import 'select2'
-import 'select2/dist/css/select2.css'
-
-window.addEventListener('DOMContentLoaded', () => {
-  $('.js-states').select2()
-})
-
 
 Rails.start()
 Turbolinks.start()
